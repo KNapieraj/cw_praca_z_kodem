2bdb6c5df5622a66eafc9e25da74cabe7463e042
Kamil Napieraj <knapieraj616@gmail.com>
2022-04-03 10:00:49 +0200

    Dodanie app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+APP = Flask(__name__)
+
+@APP.route('/')
+
+def index():
+ return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
+
+if __name__ == "__main__":
+ APP.run(debug=True)
